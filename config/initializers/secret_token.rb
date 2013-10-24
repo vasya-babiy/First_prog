6de537813ee76e74c1dcf69d036cd956c25ea3f3
vasya-babiy <salequalitypurse@gmail.com>
2013-10-24 18:31:06 +0300

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstProg::Application.config.secret_key_base = '76074dbdc3af03c3c7e09ac90c904f33dd79b834f4eec561fcc730d8d7de57957ecce51b2087df89ae88f5394585e9985551b9a2a4233a995ecf69d4f9fdfb8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstProg::Application.config.session_store :cookie_store, key: '_First_prog_session'
